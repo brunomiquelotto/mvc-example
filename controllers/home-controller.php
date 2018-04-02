66c0ef224f0758adedb0a4618e39501aba48bafa
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-04-02 08:09:17 -0300

    Estrutura básica funcionando, sem banco de dados, sem autenticação de usuário

---
+++ controllers/home-controller.php
@@ -0,0 +1,13 @@
+<?php
+
+class HomeController extends MainController
+{
+    public function index() {
+        $this->title = 'Home';
+        $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+        require ABSPATH . '/views/_includes/header.php';
+        require ABSPATH . '/views/_includes/menu.php';
+        require ABSPATH . '/views/home/index.php';
+        require ABSPATH . '/views/_includes/footer.php';
+    }
+}
